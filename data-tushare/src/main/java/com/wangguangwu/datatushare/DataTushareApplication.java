4794c47aefce28114d51a2f7e8047c7b67e853aa
wangguangwu <wangguangwu@pukanghealth.com>
2024-04-17 14:04:56 +0800

    first commit

---
+++ data-tushare/src/main/java/com/wangguangwu/datatushare/DataTushareApplication.java
@@ -0,0 +1,16 @@
+package com.wangguangwu.datatushare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangguangwu
+ */
+@SpringBootApplication
+public class DataTushareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTushareApplication.class, args);
+    }
+
+}
---
+++ data-tushare/src/test/java/com/wangguangwu/datatushare/DataTushareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.datatushare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTushareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
