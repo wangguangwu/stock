79536c61125c7295d347a5bb3aa215fa919f5747
wangguangwu <wangguangwu@pukanghealth.com>
2024-05-01 01:04:40 +0800

    报告接口

---
+++ data-snowball/src/main/java/com/wangguangwu/datasnowball/service/report/ReportFetchService.java
@@ -0,0 +1,30 @@
+package com.wangguangwu.datasnowball.service.report;
+
+import com.wangguangwu.datasnowball.constant.ReportUrlConstant;
+
+/**
+ * 获取报告数据接口
+ * <p>
+ * {@link ReportUrlConstant} 报告数据 url
+ *
+ * @author wangguangwu
+ */
+public interface ReportFetchService {
+
+    /**
+     * 获取公司最新的财务报告。
+     *
+     * @param symbol 股票代码
+     * @return 最新的财务报告数据
+     */
+    String latest(String symbol);
+
+    /**
+     * 获取公司的盈利预测报告。
+     *
+     * @param symbol 股票代码
+     * @return 盈利预测报告数据
+     */
+    String earningForecast(String symbol);
+
+}
---
+++ data-snowball/src/main/java/com/wangguangwu/datasnowball/service/report/impl/ReportFetchServiceImpl.java
@@ -0,0 +1,35 @@
+package com.wangguangwu.datasnowball.service.report.impl;
+
+import com.wangguangwu.datasnowball.component.HttpFetcherComponent;
+import com.wangguangwu.datasnowball.constant.ReportUrlConstant;
+import com.wangguangwu.datasnowball.service.report.ReportFetchService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wangguangwu
+ */
+@Service
+@Slf4j
+public class ReportFetchServiceImpl implements ReportFetchService {
+
+    @Resource
+    private HttpFetcherComponent fetcherComponent;
+
+    @Override
+    public String latest(String symbol) {
+        return fetchData(ReportUrlConstant.REPORT_LATEST_URL, symbol);
+    }
+
+    @Override
+    public String earningForecast(String symbol) {
+        return fetchData(ReportUrlConstant.REPORT_EARNING_FORECAST_URL, symbol);
+    }
+
+    //=====================================私有方法==========================================
+
+    private String fetchData(String baseUrl, String symbol) {
+        return fetcherComponent.fetchWithoutToken(baseUrl + symbol);
+    }
+}
