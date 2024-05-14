da245a542d820b6e41bb388638b068d6571a0662
wangguangwu <wangguangwu@pukanghealth.com>
2024-05-14 15:31:53 +0800

    temp save

--- backtest/src/main/java/com/wangguangwu/BackTestApplication.java
+++ backtest/src/main/java/com/wangguangwu/BackTestApplication.java
@@ -1,16 +1,12 @@
 package com.wangguangwu;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author wangguangwu
  */
 @SpringBootApplication
-@MapperScan("com.wangguangwu.backtest.mapper")
-@EnableTransactionManagement
 public class BackTestApplication {
 
         public static void main(String[] args) {
